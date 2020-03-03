f3229240ec67ef21ea04d6d1a413071f56f9fa6d
Kmbariah <Kmbaria@github.com>
2020-03-03 10:10:18 +0300

    made new changes on html, css & js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function functionName() {
   var answr=document.getElementById("george").value;
   alert(answr+" we have receive your message.Thank you!!!");
 }
+$(document).ready(function(){
+  $(".button1").click(function(){
+    $(".form1").toggle();
+  })
+})
